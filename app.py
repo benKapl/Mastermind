c667c9b10d5c84ea4c8d2453354972e833380a91
benKapl <benji1.kaplan@gmail.com>
2024-07-22 23:50:13 +0200

    started evaluate_guess() method -> unexpected behavior when at least two pairs of colors are identical

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-
-def print_red(skk): print(f"\033[91m {skk}\033[00m")
-def print_green(skk): print("\033[92m {}\033[00m" .format(skk))
-def print_yellow(skk): print("\033[93m {}\033[00m" .format(skk))
-def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
-def print_purple(skk): print("\033[95m {}\033[00m" .format(skk))
-def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+from mastermind import Game
 
 if __name__ == "__main__":
-    prCyan("Hello World, ")
-    print_yellow("coucou")
-    print_purple("It's")
-    prLightPurple("Geeks")
+    a = '\x1b[97m ■ \x1b[00m'
+    b = '\x1b[97m ■ \x1b[00m'
+
+    print(a == b)
 
 
     
--- mastermind.py
+++ mastermind.py
@@ -1,29 +1,74 @@
 import random
 
-from assets import COLORS, COLOR_RESET, CARRE 
+from assets import COLORS, COLOR_RESET, CARRE, PASTILLE, RED, WHITE
 
-class Mastermind:
+def display_colors(colors_unicode: list) -> str:
+    display = ""
+    for color in colors_unicode:
+        display += color
+    return display
+
+class Game:
     def __init__(self) -> None:
         self.computer_combination = self.generate_combination()
 
-    def generate_combination(self) -> tuple:
+    def generate_combination(self) -> list:
         """ Generate 4 random colors
 
         Returns:
-            tuple: 4 ordered colors
+            list: 4 colors
         """
-        return tuple([f"{random.choice(COLORS)} {CARRE} {COLOR_RESET}" for _ in range(4)])
+        return [f"{random.choice(COLORS)} {CARRE} {COLOR_RESET}" for _ in range(4)]
     
-    def display_combination(self, combination: tuple) -> str:
-        display = ""
-        for color in combination:
-            display += color
-        return display
+    def evaluate_guess(self, guess):
+    ### ATTENTION ###
+    # Ne fonctionne pas lorsqu'il y a plusieurs fois la même couleur identiquement placée
+    # exemple combinaison : rouge, rouge , noir, blanc
+    # exemple tentative :   rouge, rouge, blanc, bleu
+    # retournera :          pastille rouge, pastille blanc
+    # AURAIT DU RETOURNER : pastille rouge, pastille rouge, pastille blanc
+
+
+    # je zip la combinaison et le guess
+        zipped_list = list(zip(self.computer_combination, guess))
+        # je crée une liste dans laquelle j'incluerai les informations
+        evaluation = []
+        for color_combination, color_guess in zipped_list:
+            # si j'ai la bonne couleur au bon endroit
+            if color_combination == color_guess:
+                print(f"PERFECT MATCH : {display_colors(color_combination)}, {display_colors(color_guess)}")
+                # j'ajoute une pastille rouge dans ``information``
+                evaluation.append(f"{RED} {PASTILLE} {COLOR_RESET}")
+                # je retire la paire qui match parfaitement
+                zipped_list.remove((color_combination, color_guess))
+                print(f"Mise à jour des couleurs : {zipped_list}")
+                continue
+            else: 
+                print("NO PERFECT MATCH")
+        # je dezip les tuples pour comparer les couleurs restants
+        self.computer_combination, guess = zip(*zipped_list)
+        for color in guess:
+            if color in self.computer_combination:
+                print(f"Simple match: {display_colors(color)}")
+                evaluation.append(f"{WHITE} {PASTILLE} {COLOR_RESET}")
+        
+        return evaluation
+
 
 
 
 if __name__ == "__main__":
 
-    m = Mastermind()
-    a = m.display_combination(m.computer_combination)
+    game = Game()
+
+    guess = ['\x1b[95m ■ \x1b[00m', '\x1b[95m ■ \x1b[00m', '\x1b[91m ■ \x1b[00m', '\x1b[97m ■ \x1b[00m']
+
+    a = display_colors(game.computer_combination)
+    b = display_colors(guess)
+
+
     print(a)
+    print(b)
+                              
+    evaluation = game.evaluate_guess(guess)
+    print(display_colors(evaluation))
